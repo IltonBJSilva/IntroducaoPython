ea79db9a6bc2857c57d2998f60bd905d5d01902a
Ilton Batista da silva Júnior <xlsonplay@gmail.com>
2019-05-26 12:18:09 -0300

    Operadores Aritmeticos

---
+++ main.py
@@ -0,0 +1,25 @@
+#coding: utf-8
+
+a = 2
+b = 3
+
+print("a = ", 2 ,"---","b = ",b)
+
+#Soma
+print("Soma a + b: ", a+b)
+
+#Subtração
+print("Subtracao a - b: ", a-b)
+
+#b = 3
+#Exponenciação a*a*a
+print("Exponenciacao a ** b: ", a**b)
+
+#extração da parte inteira da divisão
+print("Part-inteira a // b: ", a//b)
+
+#Multiplicação
+print("Multiplicacao a * b: ", a*b)
+
+#Divisão
+print("Divisao a / b: ", a/b)

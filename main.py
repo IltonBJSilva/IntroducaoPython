50904950aa02873c49ad9cd417ef0f5a6b9fb9a3
Ilton Batista da silva Júnior <xlsonplay@gmail.com>
2019-05-26 12:18:22 -0300

    Adiciona comentario padrão

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+'''
+Nome do autor: Ilton Batista da Silva Júnior
+Data de criação do arquivo: 26/05/2019
+Objetivo sucinto do programa: Introdução a python e conceitos basicos
+Referência ao enunciado/origem do exercício: https://www.udemy.com/intro_python/learn/lecture/11785586#overview
+'''
+
 #coding: utf-8
 
 a = 2
